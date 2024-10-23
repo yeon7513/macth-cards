4e0e59b1e6985a9bd039070c18e0343abafe3270
문희연 <yeon7513@nate.com>
2024-10-23 12:34:12 +0900

    20241023-update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Board from './components/Board';
-import './scss/main.scss';
 import Intro from './components/Intro';
+import './scss/main.scss';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-          <Route path='/' element={<Intro />} />
-          <Route path='/board' element={<Board />} />
+          <Route path="/" element={<Intro />} />
+          <Route path="/board" element={<Board />} />
         </Routes>
       </div>
-
     </Router>
   );
 }
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,32 +1,30 @@
-import { combineReducers } from 'redux';
 import { createSlice } from '@reduxjs/toolkit';
-
+import { combineReducers } from 'redux';
 
 let stageReducer = createSlice({
-  name : 'STAGE',
-  initialState : {
+  name: 'STAGE',
+  initialState: {
     stage: 1,
-    boardSize: 16,
-    time: 60
+    boardSize: 8,
+    time: 60,
   },
-  reducers : {
+  reducers: {
     stageData: (state, action) => {
       const { stage, boardSize, time } = action.payload;
       state.stage = stage;
       state.boardSize = boardSize;
       state.time = time;
     },
-    setTime : (state, action) => {
+    setTime: (state, action) => {
       state.time = action.payload;
-    } 
-  }
+    },
+  },
 });
 
 export const { stageData, setTime } = stageReducer.actions;
 
 const rootReducer = combineReducers({
-  STAGE: stageReducer.reducer
-
+  STAGE: stageReducer.reducer,
 });
 
 export default rootReducer;
